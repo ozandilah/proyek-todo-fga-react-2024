511d1018d76e2c40d889fe91c6595955e2c8a274
ozandilah <ozan.it212@gmail.com>
2024-06-06 08:48:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>React Todo List</h1>
+      </header>
+      <section>
+        <h2>Ini Form Inputnya</h2>
+      </section>
+    </div>
+  );
+}
+
+export default App;
